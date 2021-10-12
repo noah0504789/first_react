f156ed97e976b9a4d1b3b45e24d3364b2e573b71
noah0504789 <noah0504@naver.com>
2021-10-12 14:18:53 +0900

    RSP & RSPHooks

--- lecture/client.jsx
+++ lecture/client.jsx
@@ -1,11 +1,13 @@
 const React = require('react')
 const ReactDom = require('react-dom')
 
-const Test = require('./RenderTest')
+// const Test = require('./RenderTest')
 // const GuGuDan = require('./GuGuDan')
 // const WordRelay = require('./WordRelay')
 // const NumberBaseball = require('./NumberBaseball')
-const NumberBaseballHooks = require('./NumberBaseballHooks')
+// const NumberBaseballHooks = require('./NumberBaseballHooks')
+const RSP = require('./RSP')
+
 
 
 
