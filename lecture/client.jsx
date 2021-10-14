247e0293edf9a1d23402d534e86bb3d972d299ed
noah0504789 <noah0504@naver.com>
2021-10-14 19:49:02 +0900

    Lotto & LottoHooks

--- lecture/client.jsx
+++ lecture/client.jsx
@@ -1,14 +1,16 @@
 const React = require('react')
 const ReactDom = require('react-dom')
 
+
+import Lotto from './Lotto';
+
 // const Test = require('./RenderTest')
 // const GuGuDan = require('./GuGuDan')
 // const WordRelay = require('./WordRelay')
 // const NumberBaseball = require('./NumberBaseball')
 // const NumberBaseballHooks = require('./NumberBaseballHooks')
-const RSP = require('./RSP')
+// const RSP = require('./RSP')
+// const Lotto = require('./Lotto')
 
 
-
-
-ReactDom.render(<NumberBaseballHooks />, document.querySelector('#root'))
+ReactDom.render(<Lotto />, document.querySelector('#root'))
