9faf5b56475b5c732e095f75e1831439316b3b72
noah0504789 <noah0504@naver.com>
2021-10-27 17:34:14 +0900

    지뢰찾기

---
+++ lecture/Tr1.jsx
@@ -0,0 +1,17 @@
+import React, {useContext, memo} from "react";
+import Td1 from './Td1'
+import {TableContext} from "./MineSearch";
+
+const Tr1 = memo(({ rowIndex }) => {
+    const { tableData } = useContext(TableContext)
+
+    return (
+        <tr>
+            {tableData[0] && Array(tableData[0].length).fill().map((td, i) =>
+                <Td1 rowIndex={rowIndex} cellIndex={i} />
+            )}
+        </tr>
+    )
+})
+
+export default Tr1
--- lecture/client.jsx
+++ lecture/client.jsx
@@ -1,7 +1,7 @@
 const React = require('react')
 const ReactDom = require('react-dom')
 
-import Lotto from './Lotto';
+import MineSearch from './MineSearch';
 
 // const Test = require('./RenderTest')
 // const GuGuDan = require('./GuGuDan')
@@ -11,4 +11,4 @@ import Lotto from './Lotto';
 // const RSP = require('./RSP')
 // const Lotto = require('./Lotto')
 
-ReactDom.render(<Lotto />, document.querySelector('#root'))
+ReactDom.render(<MineSearch />, document.querySelector('#root'))
